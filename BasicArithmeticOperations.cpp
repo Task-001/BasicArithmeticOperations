a071fac60ed3f9a7df04759f939ff510daf72bd7
JohnJoashDiaz <kraken19611@gmail.com>
2021-06-17 20:22:44 +0800

    Added Addition Between Two Numbers

---
+++ BasicArithmeticOperations.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+    using namespace std;
+    int main(){
+    double FirstNumber;
+    double SecondNumber;
+    cout <<"Enter your first Number" << endl;
+    cin  >> FirstNumber;
+    cout <<"Enter your Second Number" << endl;
+    cin  >> SecondNumber;
+    double sum = FirstNumber+SecondNumber;
+    cout <<"The Sum is : "<<sum<<endl;
+    }
